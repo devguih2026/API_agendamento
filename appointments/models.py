a43ef447059ee1d3ceb5c6b0463f687f72be5936
Guilherme Fonseca <guilhermeflor18102002@gmail.com>
2026-04-12 15:48:21 -0300

    Banco de dados

--- appointments/models.py
+++ appointments/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class Serviço(models.Model):
+    nome = models.CharField(max_length=150)
+    tempo = models.DurationField() # minutos/horas que o serviço demora
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.nome
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.nome
+
+class Agendamento(models.Model):
+    # Opções fixas para o status do agendamento
+    STATUS = [
+        ('P', 'Pendente'),
+        ('C', 'Confirmado'),
+        ('F', 'Finalizado'),
+        ('X', 'Cancelado'),
+    ]
+    data = models.DateField()
+    hora = models.TimeField()
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    serviço = models.ForeignKey(Serviço, on_delete=models.CASCADE)
+    status = models.CharField(max_length=1, choices=STATUS, default='P')
+    pagamento = models.CharField(max_length=150) # cartão ou pix
+ 
+
+
