08fdd9d570db7577f382634aa0714d0c27140534
Guilherme Fonseca <guilhermeflor18102002@gmail.com>
2026-04-13 18:44:48 -0300

    Correção urls do app

--- appointments/urls.py
+++ appointments/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from appointments import views
+from appointments.api import viewsets
 
 # O Router cria automaticamente as rotas de GET, POST, PUT e DELETE
 router = DefaultRouter()
-router.register(r'agendamentos', views.Agendamentoviewsets, basename='agendamento')
+router.register(r'agendamentos', viewsets.Agendamentoviewsets , basename='agendamento')
 
 urlpatterns = [
     path('', include(router.urls)),
