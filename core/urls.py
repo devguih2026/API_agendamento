f3a31324349296c4d2e5b0685f8ac22c686a2843
Guilherme Fonseca <guilhermeflor18102002@gmail.com>
2026-04-17 18:03:00 -0300

    URLs para testes no postman

--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Rota para o seu APP
@@ -26,4 +31,8 @@ urlpatterns = [
     # Rotas para a Documentação Automática (Swagger)
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+ 
+    # teste no postman
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
