aa0602cd7afc33f4e8b0bf73df7a485006419c9f
Guilherme Fonseca <guilhermeflor18102002@gmail.com>
2026-04-17 18:53:58 -0300

    URLs de clientes e serviços para testar no postman

--- appointments/urls.py
+++ appointments/urls.py
@@ -5,6 +5,8 @@ from appointments.api import viewsets
 # O Router cria automaticamente as rotas de GET, POST, PUT e DELETE
 router = DefaultRouter()
 router.register(r'agendamentos', viewsets.Agendamentoviewsets , basename='agendamento')
+router.register(r'clientes', viewsets.ClienteViewsets, basename='cliente')
+router.register(r'servicos', viewsets.ServicoViewsets, basename='servico')
 
 urlpatterns = [
     path('', include(router.urls)),
